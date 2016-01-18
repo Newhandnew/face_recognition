98da3899efacc8e1cb86331fd5f468081f085126
newhandnew <easytired@gmail.com>
2016-01-18 15:09:09 +0800

    add a new image transfer topic to seperate showing image and main server

--- face_recognition.sh
+++ face_recognition.sh
@@ -1,2 +1,2 @@
 roslaunch face_recognition face_recognition_camera.launch &
-rosrun face_recognition Fserver _confidence_value:=0.85
+rosrun face_recognition Fserver _confidence_value:=0.85 _show_screen_flag:=false
